7d15349dee433bc72d31792628ef916a9690fbbd
Rajesh <rajeshmanik721211@gmail.com>
2026-02-12 19:33:34 +0530

    PreCommit

---
+++ SpringDataJPA4th/src/main/java/com/rajesh/SpringDataJpa4thApplication.java
@@ -0,0 +1,13 @@
+package com.rajesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpa4thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpa4thApplication.class, args);
+	}
+
+}
